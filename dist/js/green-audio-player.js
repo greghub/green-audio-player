15d2488de74f740472e410dd6bd2999ae9abb554
tnbnicer <49583362+tnbnicer@users.noreply.github.com>
2021-08-21 20:34:10 +0100

    Revert "Bug Issue #30 patch"
    This reverts commit 6b3f7f1da65bf143800ca962d9f109178ecc8767.

--- dist/js/green-audio-player.js
+++ dist/js/green-audio-player.js
@@ -11,15 +11,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -242,7 +238,7 @@ var GreenAudioPlayer = /*#__PURE__*/function () {
   }, {
     key: "inRange",
     value: function inRange(event) {
-      var touch = ('touches' in event); // instanceof TouchEvent may also be used
+      var touch = 'touches' in event; // instanceof TouchEvent may also be used
 
       var rangeBox = this.getRangeBox(event);
       var sliderPositionAndDimensions = rangeBox.getBoundingClientRect();
@@ -310,7 +306,7 @@ var GreenAudioPlayer = /*#__PURE__*/function () {
   }, {
     key: "getCoefficient",
     value: function getCoefficient(event) {
-      var touch = ('touches' in event); // instanceof TouchEvent may also be used
+      var touch = 'touches' in event; // instanceof TouchEvent may also be used
 
       var slider = this.getRangeBox(event);
       var sliderPositionAndDimensions = slider.getBoundingClientRect();
@@ -601,13 +597,6 @@ var GreenAudioPlayer = /*#__PURE__*/function () {
 
       for (var i = 0; i < players.length; i++) {
         GreenAudioPlayer.pausePlayer(players[i]);
-        var holderDiv = players[i].parentElement.querySelector('.holder');
-        var playPauseBtn = holderDiv.querySelector('.play-pause-btn');
-        playPauseBtn.setAttribute('aria-label', 'Play');
-
-        if (playPauseBtn.attributes.title) {
-          playPauseBtn.setAttribute('title', 'Play');
-        }
       }
     }
   }]);
