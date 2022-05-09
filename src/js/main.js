cb0727c98a18571136e5020788bfbe7cfc7a0ffc
tnbnicer <49583362+tnbnicer@users.noreply.github.com>
2022-05-09 18:49:51 +0100

    Update main.js
    1) New "isDevice" definition
    tests "uaDataIsMobile", "isTouch", and, if required, User Agent sniffs.
    2) "isSafari" definition
    There should be two detections, "isDevice" and "isSafari"
    3) overcomeIosLimitations ->>> overcomeAppleLimitations
    It is not an iOS limitation but a Safari one, an Apple policy to disable "canplay".
    4) Hide volume controls on a touch screen devices universally, not just on iOS, Android.
    5) Added non-passive event listener to a scroll-blocking ‘touchstart’ event. There was a console warning to the effect that marking the event handler as "passive" would make the page more responsive. Okay.
    6) new listener for when the player page is re-focused. On mobiles the play button is then refreshed. A playing player will be paused when another audio starts, so button state needs to reflect this. Play ->>> Pause.

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,20 @@ class GreenAudioPlayer {
         const audioElement = this.audioPlayer.innerHTML;
         this.audioPlayer.classList.add('green-audio-player');
         this.audioPlayer.innerHTML = GreenAudioPlayer.getTemplate() + audioElement;
+        const isTouch = window.matchMedia
+        && window.matchMedia('(pointer:coarse)').matches; // primary pointer is touch
+        const isDesktopIE11 = window.MSInputMethodContext && document.documentMode
+        && /tablet/i.test(window.navigator.userAgent.toLowerCase());
         const uaDataIsMobile = window.navigator.userAgentData
         && window.navigator.userAgentData.mobile;
-        this.isDevice = typeof uaDataIsMobile === 'boolean'
-            ? uaDataIsMobile
-            : (/ipad|iphone|ipod|android/i.test(window.navigator.userAgent.toLowerCase())
-            || (window.navigator.platform === 'MacIntel' && window.navigator.maxTouchPoints > 1))
-            && !window.MSStream;
+        this.isSafari = typeof uaDataIsMobile === 'boolean' // browser supports "Client Hints"
+            ? window.navigator.userAgentData.brands[1]
+            && window.navigator.userAgentData.brands[1].brand.indexOf('Safari') // check if Safari brand exists
+            : /^((?!chrom|android|kindle|crios|fxios).)*version.+safari/i.test(window.navigator.userAgent.toLowerCase());
+        this.isDevice = typeof uaDataIsMobile === 'boolean' // browser supports "Client Hints"
+            ? window.navigator.userAgentData.mobile || isTouch // all mobiles and tablets
+            : isTouch || (!isDesktopIE11 // IE desktop's User Agent string claims to be "Tablet"
+            && /android|iphone|ipod|ipad|tablet/i.test(window.navigator.userAgent.toLowerCase())); // test User Agents here
         this.playPauseBtn = this.audioPlayer.querySelector('.play-pause-btn');
         this.loading = this.audioPlayer.querySelector('.loading');
         this.sliders = this.audioPlayer.querySelectorAll('.slider');
@@ -83,7 +90,12 @@ class GreenAudioPlayer {
 
         this.initEvents();
         this.directionAware();
-        this.overcomeIosLimitations();
+        this.overcomeAppleLimitations();
+        this.disableVolumeControls(); // mouseless devices
+
+        if ('data-duration' in this.player.attributes) {
+            self.totalTime.textContent = GreenAudioPlayer.formatTime(this.player.getAttribute('data-duration'));
+        }
 
         if ('autoplay' in this.player.attributes) {
             const promise = this.player.play();
@@ -214,8 +226,16 @@ class GreenAudioPlayer {
 
                 event.preventDefault();
             }
-        });
+        }, { passive: true });
 
+        window.addEventListener('visibilitychange', () => {
+            if (!document.hidden && self.player.paused === true
+                && self.player.duration >= 0) {
+                GreenAudioPlayer.pausePlayer(self.player, 'toggle');
+                self.playPauseBtn.setAttribute('aria-label', self.labels.play);
+                self.hasSetAttribute(self.playPauseBtn, 'title', self.labels.play);
+            }
+        });
         this.playPauseBtn.addEventListener('click', this.togglePlay.bind(self));
         this.player.addEventListener('timeupdate', this.updateProgress.bind(self));
         this.player.addEventListener('volumechange', this.updateVolume.bind(self));
@@ -247,12 +267,18 @@ class GreenAudioPlayer {
         this.downloadLink.addEventListener('contextmenu', this.downloadAudio.bind(self));
     }
 
-    overcomeIosLimitations() {
+    overcomeAppleLimitations() {
         const self = this;
-        if (this.isDevice) {
-            // iOS does not support "canplay" event
+        if (this.isSafari) {
+            // MacOS and iOS Safari do not support "canplay" event
             this.player.addEventListener('loadedmetadata', this.hideLoadingIndicator.bind(self));
-            // iOS does not let "volume" property to be set programmatically
+        }
+    }
+
+    disableVolumeControls() {
+        if (this.isDevice) {
+            // you need a mouse for dragging the slider
+            // iOS also does not allow "volume" property to be set programmatically
             this.audioPlayer.querySelector('.volume').style.display = 'none';
             this.audioPlayer.querySelector('.controls').style.marginRight = '0';
         }
